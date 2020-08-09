c68734656e2105fc53678c7b2a7cc16f2040b426
alireza.paladin <alireza.abdolalipour@gmail.com>
2020-08-09 23:29:39 +0430

    add question and answers generator

--- javascriptMathGame.js
+++ javascriptMathGame.js
@@ -1,4 +1,3 @@
-alert('hello')
 var gameFlag=false
 var timecounter=60
 function startGame() {
@@ -15,10 +14,42 @@ function startGame() {
 
     else {
         document.getElementById('commandButton').innerHTML='Start Game'
+        gameFlag=false
+        clearInterval(t)
+        clearTimeout(timer)
+        timecounter=60
+        document.getElementById('timeRemain').innerHTML='0'
     }
-
-
 }
+
+window.console.log(randomAnswer())
+
+
+function randomAnswer() {
+    var quesAnswer= new Array(2)
+    for (i=0;i<4;i++) {
+        var num1 = Math.floor(Math.random() * 10)
+        var num2 = Math.floor(Math.random() * 10)
+        quesAnswer[i]=new Array(2)
+        quesAnswer[i][0]=num1
+        quesAnswer[i][1]=num2
+        quesAnswer[i][2]=num1*num2
+    }
+    var choiser=Math.floor(Math.random()*4)
+
+
+
+     document.getElementById('answer1').innerHTML=quesAnswer[0][2]
+     document.getElementById('answer2').innerHTML=quesAnswer[1][2]
+     document.getElementById('answer3').innerHTML=quesAnswer[2][2]
+     document.getElementById('answer4').innerHTML=quesAnswer[3][2]
+
+    document.getElementById('question').innerHTML=quesAnswer[choiser][0]+' x '+quesAnswer[choiser][1]
+    return choiser
+}
+
+
+
 function gameover() {
     document.getElementById('gameOver').style.display='block'
     document.getElementById('gameOver').innerHTML='GAME OVER & Your score is '
@@ -29,7 +60,7 @@ function gameover() {
 }
 function timeShow() {
     timecounter-=1
-    document.getElementById('testResult').innerHTML=timecounter
+    document.getElementById('timeRemain').innerHTML=timecounter
     if (gameFlag===false || timecounter<=0 ){
         clearInterval(t)
         timecounter=60
